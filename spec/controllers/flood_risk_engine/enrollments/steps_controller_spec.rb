56da484f61e59797045620389d9a3d01aae77937
Rob Nichols <robnichols.ea@gmail.com>
2016-04-11 14:07:14 +0100

    Add step controller test

---
+++ spec/controllers/flood_risk_engine/enrollments/steps_controller_spec.rb
@@ -0,0 +1,52 @@
+require "rails_helper"
+describe FloodRiskEngine::Enrollments::StepsController, type: :controller do
+  routes { FloodRiskEngine::Engine.routes }
+  render_views
+
+  let(:enrollment) { FloodRiskEngine::Enrollment.create }
+
+  context "step 1" do
+    let(:step) { "step1" }
+
+    before do
+      get :edit, step: step, id: enrollment
+    end
+
+    it "uses Step1Form" do
+      expect(controller.send(:form)).to be_a(FloodRiskEngine::Steps::Step1Form)
+    end
+
+    it "diplays header" do
+      header_text = "Question 1"
+      expect(response.body).to have_tag :h1, text: /#{header_text}/
+    end
+  end
+
+  context "step 2" do
+    let(:step) { "step2" }
+
+    it "uses Step2Form" do
+      get :edit, step: step, id: enrollment
+      expect(controller.send(:form)).to be_a(FloodRiskEngine::Steps::Step2Form)
+    end
+  end
+
+  context "step 3" do
+    let(:step) { "step3" }
+
+    it "uses Step3Form" do
+      get :edit, step: step, id: enrollment
+      expect(controller.send(:form)).to be_a(FloodRiskEngine::Steps::Step3Form)
+    end
+  end
+
+  context "step unknown" do
+    let(:step) { "unknown" }
+
+    it "uses Step1Form" do
+      expect do
+        get :edit, step: step, id: enrollment
+      end.to raise_error(StandardError)
+    end
+  end
+end
