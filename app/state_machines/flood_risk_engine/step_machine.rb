b5bd83d1d41cdb0bd6f4bb9dcd7dbbae5b040f79
Rob Nichols <robnichols.ea@gmail.com>
2016-04-13 11:44:14 +0100

    Add decorator (StepMachine) between Enrollment and StateMachine

---
+++ app/state_machines/flood_risk_engine/step_machine.rb
@@ -0,0 +1,74 @@
+module FloodRiskEngine
+  class StepMachine
+    attr_reader :host, :state_machine_class, :step_method, :history_method
+    def initialize  host:,
+                    state_machine_class:,
+                    step_method: :step,
+                    history_method: :step_history
+      @host = host
+      @state_machine_class = state_machine_class
+      @step_method = step_method
+      @history_method = history_method
+    end
+
+    def current_step
+      state.to_s
+    end
+
+    def set_step_as(step)
+      restore!(step)
+      step_history << step
+    end
+
+    def rollback_to(step)
+      rollback_valid_with? step
+      set_step_as step
+      self.step_history = step_history.take_while {|s| s != step.to_sym}
+    end
+
+    def previous_step?(step)
+      step_history.last == step.to_sym
+    end
+
+    def defined_steps
+      states.collect(&:to_s)
+    end
+
+    def state_machine
+      @state_machine ||= initiate_state_machine
+    end
+    delegate(
+      :next_step, :state, :restore!, :states,
+      to: :state_machine
+    )
+
+    private
+    def initiate_state_machine
+      state_machine = state_machine_class.new
+      state_machine.target(host)
+      state_machine.restore!(host_step) if host_step?
+      state_machine
+    end
+
+    def host_step
+      host.send step_method
+    end
+
+    def host_step?
+      host_step.present?
+    end
+
+    def step_history
+      host.send history_method
+    end
+
+    def step_history=(steps)
+      host.send "#{history_method}=", steps
+    end
+
+    def rollback_valid_with?(step)
+      return if step_history.include?(step.to_sym)
+      raise StateMachineError, "Cannot rollback to step unless in history"
+    end
+  end
+end
--- spec/support/state_machines/test_state_machine.rb
+++ spec/support/state_machines/test_state_machine.rb
@@ -1,14 +1,16 @@
+# rubocop:disable Style/HashSyntax
 module FloodRiskEngine
   class TestStateMachine < StateMachine
     initial :step1
 
     events do
       event :next_step, if: -> { target.business_type == :foo },
-        step1: :step2,
-        step2: :step3
+        :step1 => :step2,
+        :step2 => :step3
 
       event :next_step, if: -> { target.business_type == :bar },
-        step1: :step3
+        :step1 => :step3
     end
   end
 end
+# rubocop:enable Style/HashSyntax
