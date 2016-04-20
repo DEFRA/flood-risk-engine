baad5830e6aecb5f3d82bc57f3d3cbe689c42714
Rob Nichols <robnichols.ea@gmail.com>
2016-04-20 10:29:20 +0100

    Make organisation factory more generic

--- spec/factories/organisations.rb
+++ spec/factories/organisations.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :organisation, class: "FloodRiskEngine::Organisation" do
-    org_type :other
+    org_type 0
     name Faker::Company.name
     contact_id 1
     company_number Faker::Number.number(8)
