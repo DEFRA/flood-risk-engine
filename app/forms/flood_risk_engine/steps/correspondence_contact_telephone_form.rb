5c36950dab18854c5a323c28f059760ac11c8c49
Rob Nichols <git@nicholshayes.co.uk>
2016-05-09 14:26:24 +0100

    Replace files removed by rebase

---
+++ app/forms/flood_risk_engine/steps/correspondence_contact_telephone_form.rb
@@ -0,0 +1,38 @@
+
+
+module FloodRiskEngine
+  module Steps
+    class CorrespondenceContactTelephoneForm < BaseForm
+
+      def self.factory(enrollment)
+        enrollment.correspondence_contact ||= FloodRiskEngine::Contact.new(contact_type: :correspondence)
+        new(enrollment.correspondence_contact, enrollment)
+      end
+
+      def self.params_key
+        :correspondence_contact_telephone
+      end
+
+      property :telephone_number
+
+      # phone validation from https://github.com/daddyz/phonelib
+      validates(
+        :telephone_number,
+        phone: {
+          message: I18n.t("#{locale_key}.errors.telephone_number.invalid"),
+          allow_blank: true
+        },
+        presence: {
+          message: I18n.t("#{locale_key}.errors.telephone_number.blank")
+        }
+      )
+
+      def save
+        super
+        enrollment.correspondence_contact ||= model
+        enrollment.save
+      end
+
+    end
+  end
+end
