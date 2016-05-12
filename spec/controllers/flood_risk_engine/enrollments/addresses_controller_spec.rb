36540c83a58e4e4587307db93d51885ff4f87249
Rob Nichols <robnichols.ea@gmail.com>
2016-05-12 16:16:04 +0100

    Add tests to addresses controller spec

--- spec/controllers/flood_risk_engine/enrollments/addresses_controller_spec.rb
+++ spec/controllers/flood_risk_engine/enrollments/addresses_controller_spec.rb
@@ -1,9 +1,22 @@
 require "rails_helper"
 
 module FloodRiskEngine
-  RSpec.describe Enrollments::AddressesController, type: :controller do
-    routes { Engine.routes }
-    render_views
-    let(:enrollment) { FactoryGirl.create(:enrollment) }
+  module Enrollments
+    RSpec.describe AddressesController, type: :controller do
+      routes { Engine.routes }
+      render_views
+      let(:enrollment) { FactoryGirl.create(:enrollment) }
+      let(:address) { FactoryGirl.create(:address) }
+
+      describe "show action" do
+        before do
+          get :show, id: address, enrollment_id: enrollment
+        end
+
+        it "should render page sucessfully" do
+          expect(response).to have_http_status(:success)
+        end
+      end
+    end
   end
 end
