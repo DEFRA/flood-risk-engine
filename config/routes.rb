fb98e7dda149b70947efb7149a86bc501e82de61
tom statter <github@autotelik.co.uk>
2016-05-18 18:07:21 +0100

    fix bad route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 FloodRiskEngine::Engine.routes.draw do
   resources :enrollments, only: [:new, :create] do
-    resources :steps, only: [:show, :update], controller: "enrollments/steps" do
-    end
+    resources :steps, only: [:show, :update], controller: "enrollments/steps"
 
     resources :exemptions, only: [:destroy, :show], controller: "enrollments/exemptions"
   end
