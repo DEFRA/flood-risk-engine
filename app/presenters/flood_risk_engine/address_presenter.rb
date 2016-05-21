9f28776b15f075700cc717dc0d1acd9b6d139252
Tim Crowe <tim@woodpigeon.com>
2016-05-21 21:28:21 +0100

    WIP

--- app/presenters/flood_risk_engine/address_presenter.rb
+++ app/presenters/flood_risk_engine/address_presenter.rb
@@ -5,6 +5,8 @@ module FloodRiskEngine
       @address = address
     end
 
+    # Returns a friendly single line representation of the address, e.g.
+    # Flat 1A, 121 Acacia Avenue, Broadbean, Kent, TU4 8AA
     def to_s
       return unless address
       %i(premises street_address locality city postcode).map do |attribute|
