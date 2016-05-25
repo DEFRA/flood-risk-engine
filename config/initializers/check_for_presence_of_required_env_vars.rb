f960b80524313a712c5ed7adcaefb68aed373b58
Tim Crowe <tim@woodpigeon.com>
2016-05-25 12:58:06 +0100

    Revert to using ADDRESS_FACADE_* vars across all environments
    Having ADDRESS_FACADE_TEST_* variables for dev and test environments
    was adding uneccesary complexity in the engine and front end.

--- config/initializers/check_for_presence_of_required_env_vars.rb
+++ config/initializers/check_for_presence_of_required_env_vars.rb
@@ -2,8 +2,8 @@
 # errors, for example email failing on first execution because of missing configuration.
 # You can add these variables in development or CI using your .env file.
 %w(SECRET_KEY_BASE
-   ADDRESS_FACADE_TEST_SERVER
-   ADDRESS_FACADE_TEST_PORT
+   ADDRESS_FACADE_SERVER
+   ADDRESS_FACADE_PORT
    ADDRESS_FACADE_CLIENT_ID
    ADDRESS_FACADE_KEY).each do |key|
   ENV.fetch(key) { raise "#{key} not found in ENV" }
