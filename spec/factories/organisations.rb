a05af15cd209a086feb96e63c553f50e4915fdd6
tom statter <github@autotelik.co.uk>
2016-06-13 13:19:19 +0100

    rspecs

--- spec/factories/organisations.rb
+++ spec/factories/organisations.rb
@@ -8,10 +8,12 @@ FactoryGirl.define do
       object.contact = FloodRiskEngine::Contact.create(contact_type: :establishment_or_undertaking)
     end
 
-    # Create a trait for each Type, in format : as_local_authority
+    # A trait to create a blank Organisation for each Type
+    # One trait per type named in format : as_xxx e.g   as_local_authority, as_limited_liability_partnership etc
     FloodRiskEngine::Organisation.org_types.keys.each do |ot|
       trait :"as_#{ot}" do
         org_type ot.to_s
+        name ""
         registration_number ""
       end
     end
