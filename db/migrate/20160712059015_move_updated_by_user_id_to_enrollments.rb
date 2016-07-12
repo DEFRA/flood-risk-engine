b1f978ef6f634080fda47b787694c22b01639ef3
tom statter <github@autotelik.co.uk>
2016-07-12 16:05:07 +0000

    Fix migrations for existing installs

--- db/migrate/20160712059015_move_updated_by_user_id_to_enrollments.rb
+++ db/migrate/20160712059015_move_updated_by_user_id_to_enrollments.rb
@@ -2,16 +2,13 @@ class MoveUpdatedByUserIdToEnrollments < ActiveRecord::Migration
   def change
 
     # Migration moved here from BO so specs can be run but so existing install don't break
-    # we replay as a later  migration 
+    # we replay as a later  migration
 
     if(column_exists?(:flood_risk_engine_enrollments, :updated_by_user_id))
       remove_column :flood_risk_engine_enrollments, :updated_by_user_id, :integer
-
-      add_column :flood_risk_engine_enrollments, :updated_by_user_id, :integer
-      add_index :flood_risk_engine_enrollments, :updated_by_user_id
-
-      add_foreign_key :flood_risk_engine_enrollments, :users, column: :updated_by_user_id
     end
 
+    add_column :flood_risk_engine_enrollments, :updated_by_user_id, :integer
+    add_index :flood_risk_engine_enrollments, :updated_by_user_id
   end
 end
