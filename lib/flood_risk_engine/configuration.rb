261eac5bc0f0bc38257b33cfdb3a8c547c667e6e
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2020-02-20 09:11:57 +0000

    Mount defra ruby email and handle config

--- lib/flood_risk_engine/configuration.rb
+++ lib/flood_risk_engine/configuration.rb
@@ -74,6 +74,13 @@ module FloodRiskEngine
       end
     end
 
+    # Last Email caching and retrieval functionality
+    def use_last_email_cache=(value)
+      DefraRubyEmail.configure do |configuration|
+        configuration.enable = change_string_to_boolean_for(value)
+      end
+    end
+
     private
 
     # If the setting's value is "true", then set to a boolean true. Otherwise, set it to false.
