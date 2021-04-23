714c2229d51f2cb929ca4aefc6dc671b522a4321
Toby Privett <toby@snaplab.co.uk>
2021-04-23 15:12:25 +0100

    TEMP: Disable defra_ruby_email - no rails 5 version

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ FloodRiskEngine::Engine.routes.draw do
     )
   end
 
-  mount DefraRubyEmail::Engine => "/email"
+  # mount DefraRubyEmail::Engine => "/email"
 
   # See http://patrickperey.com/railscast-053-handling-exceptions/
   get "(errors)/:id", to: "errors#show", as: "error"
