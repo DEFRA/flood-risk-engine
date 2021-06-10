c071fdd046b07d9093b3e70cefb48ec0c10f5309
irisfaraway <iris.faraway@gmail.com>
2021-06-10 14:08:16 +0100

    Add missing address config

--- config/initializers/defra_ruby_address.rb
+++ config/initializers/defra_ruby_address.rb
@@ -3,6 +3,7 @@
 require "defra_ruby/address"
 
 DefraRuby::Address.configure do |configuration|
+  configuration.host = ENV["ADDRESSBASE_URL"] || "http://localhost:9002"
   configuration.key = ENV["ADDRESS_FACADE_CLIENT_KEY"]
   configuration.client_id = ENV["ADDRESS_FACADE_CLIENT_ID"]
 end
