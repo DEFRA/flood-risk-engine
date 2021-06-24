d62e989289bf7033618cddecc039c286dfc15291
irisfaraway <iris.faraway@gmail.com>
2021-06-24 13:21:22 +0100

    Create form objects
    In future PRs these will be modified to deal with validations, etc. However for now we just need them to      submit.

---
+++ app/forms/flood_risk_engine/business_type_form.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module FloodRiskEngine
+  class BusinessTypeForm < ::FloodRiskEngine::BaseForm
+    def submit(_params)
+      # Assign the params for validation and pass them to the BaseForm method for updating
+      attributes = {}
+
+      super(attributes)
+    end
+  end
+end
