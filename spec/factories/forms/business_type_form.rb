e48602d66ba4f40bd86cfdf9c26888b4003c023b
irisfaraway <iris.faraway@gmail.com>
2021-07-10 21:44:17 +0100

    Add form validation for business_type

---
+++ spec/factories/forms/business_type_form.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :business_type_form, class: FloodRiskEngine::BusinessTypeForm do
+    trait :has_required_data do
+      initialize_with do
+        new(
+          create(
+            :new_registration,
+            business_type: "limitedCompany",
+            workflow_state: "business_type_form"
+          )
+        )
+      end
+    end
+  end
+end
