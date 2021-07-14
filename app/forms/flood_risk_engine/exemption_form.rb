a9aa5b89c9710e6a31a49ca9276d282e827ff365
Toby Privett <toby@snaplab.co.uk>
2021-07-14 16:08:44 +0100

    WIP Form: Select the exemption you want to register
    - We need to assign the exemption to the transient registration
    - Currently, there is a has_many relationship but this could (should?)
    be a has_one or belongs_to

--- app/forms/flood_risk_engine/exemption_form.rb
+++ app/forms/flood_risk_engine/exemption_form.rb
@@ -5,5 +5,14 @@ module FloodRiskEngine
     delegate :exemptions, to: :transient_registration
 
     validates :exemptions, "flood_risk_engine/exemptions": true
+
+    attr_accessor :exemption_id
+
+    def all_exemptions
+      @all_exemptions ||=
+        Exemption.all.map do |exemption|
+          ExemptionPresenter.new(exemption)
+        end
+    end
   end
 end
