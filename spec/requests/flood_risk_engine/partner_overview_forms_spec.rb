26d7c33f19acf901a3c5d36af14ce3bbb1f7b362
irisfaraway <iris.faraway@gmail.com>
2021-08-15 18:17:52 +0100

    Fix copy-paste error

--- spec/requests/flood_risk_engine/partner_overview_forms_spec.rb
+++ spec/requests/flood_risk_engine/partner_overview_forms_spec.rb
@@ -3,21 +3,21 @@
 require "rails_helper"
 
 module FloodRiskEngine
-  RSpec.describe "PartnerNameForms", type: :request do
-    include_examples "GET flexible form", "partner_name_form"
+  RSpec.describe "PartnerOverviewForms", type: :request do
+    include_examples "GET flexible form", "partner_overview_form"
 
-    include_examples "POST without params form", "partner_name_form"
+    include_examples "POST without params form", "partner_overview_form"
 
-    describe "GET back_partner_name_forms_path" do
+    describe "GET back_partner_overview_forms_path" do
       context "when a valid transient registration exists" do
         let(:transient_registration) do
           create(:new_registration,
-                 workflow_state: "partner_name_form")
+                 workflow_state: "partner_overview_form")
         end
 
         context "when the back action is triggered" do
           it "returns a 302 response and redirects to the business_type form" do
-            get back_partner_name_forms_path(transient_registration[:token])
+            get back_partner_overview_forms_path(transient_registration[:token])
 
             expect(response).to have_http_status(302)
             expect(response).to redirect_to(new_business_type_form_path(transient_registration[:token]))
@@ -33,7 +33,7 @@ module FloodRiskEngine
 
         context "when the back action is triggered" do
           it "returns a 302 response and redirects to the correct form for the state" do
-            get back_partner_name_forms_path(transient_registration[:token])
+            get back_partner_overview_forms_path(transient_registration[:token])
 
             expect(response).to have_http_status(302)
             expect(response).to redirect_to(new_declaration_form_path(transient_registration[:token]))
