8654b50fb3668379bdfcced59b0814588a55db26
Iris Faraway <iris.faraway@gmail.com>
2021-09-16 15:44:04 +0100

    Remove TabularEnrollmentDetailPresenters (#419)
    These are no longer in use for the check your answers page, and despite the comments weren't in use for the emails either.

--- app/mailers/flood_risk_engine/enrollment_mailer.rb
+++ app/mailers/flood_risk_engine/enrollment_mailer.rb
@@ -3,11 +3,8 @@ module FloodRiskEngine
     def submitted(enrollment_id:, recipient_address:)
       i18n_scope = "flood_risk_engine.enrollment_mailer.submitted"
       subject = I18n.t(".subject", scope: i18n_scope)
-      underlying_enrollment = Enrollment.find(enrollment_id)
-      @enrollment = TabularEnrollmentDetailPresenter.new(enrollment: underlying_enrollment,
-                                                         i18n_scope: i18n_scope,
-                                                         display_change_url: false)
-      @exemption = underlying_enrollment.exemptions.first
+      @enrollment = Enrollment.find(enrollment_id)
+      @exemption = @enrollment.exemptions.first
 
       mail to: recipient_address,
            from: ENV["DEVISE_MAILER_SENDER"],
--- spec/presenters/flood_risk_engine/tabular_enrollment_detail_presenter_spec.rb
+++
@@ -1,8 +0,0 @@
-require "rails_helper"
-
-module FloodRiskEngine
-  RSpec.describe TabularEnrollmentDetailPresenter, type: :presenter do
-    let(:enrollment) { build_stubbed(:enrollment) } # , token: "123") }
-    subject { described_class.new(enrollment) }
-  end
-end
