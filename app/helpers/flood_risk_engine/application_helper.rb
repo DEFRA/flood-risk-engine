792e666d9aa46068770af91e940db7cbf24260a4
irisfaraway <iris.faraway@gmail.com>
2021-09-17 14:15:16 +0100

    Get title text from locale files automatically

--- app/helpers/flood_risk_engine/application_helper.rb
+++ app/helpers/flood_risk_engine/application_helper.rb
@@ -1,17 +1,10 @@
 module FloodRiskEngine
   module ApplicationHelper
-    def page_title(title)
-      return unless title.present?
-
-      stripped_title = title.gsub(/’/, %('))
-
-      if content_for? :page_title
-        content_for :page_title, " - #{stripped_title} - GOV.UK"
-      else
-        content_for :page_title, stripped_title.to_s
-      end
-
-      title
+    def title
+      title_elements = [title_text, "Register a flood risk activity exemption", "GOV.UK"]
+      # Remove empty elements, for example if no specific title is set
+      title_elements.delete_if(&:empty?)
+      title_elements.join(" - ")
     end
 
     def displayable_address(address)
@@ -24,6 +17,21 @@ module FloodRiskEngine
        address.locality,
        address.city,
        address.postcode].reject(&:blank?)
+    end
+
+    private
+
+    def title_text
+      # Check if the title is set in the view (we do this for High Voltage pages)
+      return content_for :title if content_for?(:title)
+
+      # Otherwise, look up translation key based on controller path, action name and .title
+      # Solution from https://coderwall.com/p/a1pj7w/rails-page-titles-with-the-right-amount-of-magic
+      title = t("#{controller_path.tr('/', '.')}.#{action_name}.title", default: "")
+      return title if title.present?
+
+      # Default to title for "new" action if the current action doesn't return anything
+      t("#{controller_path.tr('/', '.')}.new.title", default: "")
     end
   end
 end
