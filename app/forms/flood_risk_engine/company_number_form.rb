e90afbd8707f7bb945bbc876442fa22c5dde824d
irisfaraway <iris.faraway@gmail.com>
2021-09-20 13:29:36 +0100

    Don't append extra 0s to short Companies House number
    https://eaflood.atlassian.net/browse/RUBY-1462
    
    This is something we do in WCR and WEX as the number then gets sent off to Companies House for validation. However here it just messes up the checks we do on the format, so we'd prefer to just leave the input as-is.

--- app/forms/flood_risk_engine/company_number_form.rb
+++ app/forms/flood_risk_engine/company_number_form.rb
@@ -6,27 +6,8 @@ module FloodRiskEngine
 
     validates :company_number, "flood_risk_engine/company_number": true
 
-    def submit(params)
-      # Assign the params for validation and pass them to the BaseForm method for updating
-      # If param isn't set, use a blank string instead to avoid errors with the validator
-      params[:company_number] = process_company_number(params[:company_number])
-
-      super
-    end
-
     def business_type
       FloodRiskEngine::TransientRegistration::BUSINESS_TYPES.key(transient_registration.business_type)
-    end
-
-    private
-
-    def process_company_number(company_number)
-      return unless company_number.present?
-
-      number = company_number.to_s
-      # Should be 8 characters, so if it's not, add 0s to the start
-      number = "0#{number}" while number.length < 8
-      number
     end
   end
 end
