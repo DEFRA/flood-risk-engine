efa1ee299033a4ead7a550c611daea319513dd29
Iris Faraway <iris.faraway@gmail.com>
2021-09-23 09:54:58 +0100

    Validate company number with Companies House (#435)
    https://eaflood.atlassian.net/browse/RUBY-1506
    
    Previously we just validated the format of the Companies House number to see if it looked about right. Now we use our validation gem to send it off to the Companies House API to check if it actually belongs to an active company.
    
    Also ripped out some old validators that are no longer used.

--- app/forms/flood_risk_engine/company_number_form.rb
+++ app/forms/flood_risk_engine/company_number_form.rb
@@ -4,10 +4,29 @@ module FloodRiskEngine
   class CompanyNumberForm < ::FloodRiskEngine::BaseForm
     delegate :company_number, to: :transient_registration
 
-    validates :company_number, "flood_risk_engine/company_number": true
+    validates :company_number, "defra_ruby/validators/companies_house_number": true
 
     def business_type
       FloodRiskEngine::TransientRegistration::BUSINESS_TYPES.key(transient_registration.business_type)
+    end
+
+    def submit(params)
+      # Assign the params for validation and pass them to the BaseForm method for updating
+      # If param isn't set, use a blank string instead to avoid errors with the validator
+      params[:company_number] = process_company_number(params[:company_number])
+
+      super
+    end
+
+    private
+
+    def process_company_number(company_number)
+      return unless company_number.present?
+
+      number = company_number.to_s
+      # Should be 8 characters, so if it's not, add 0s to the start
+      number = "0#{number}" while number.length < 8
+      number
     end
   end
 end
--- spec/forms/flood_risk_engine/company_number_forms_spec.rb
+++ spec/forms/flood_risk_engine/company_number_forms_spec.rb
@@ -4,15 +4,32 @@ require "rails_helper"
 
 module FloodRiskEngine
   RSpec.describe CompanyNumberForm, type: :model do
+    before do
+      allow_any_instance_of(DefraRuby::Validators::CompaniesHouseService).to receive(:status).and_return(:active)
+    end
+
     describe "#submit" do
       context "when the form is valid" do
         let(:company_number_form) { build(:company_number_form, :has_required_data) }
         let(:valid_params) do
-          { token: company_number_form.token, company_number: company_number_form.company_number }
+          { company_number: company_number_form.company_number }
         end
 
         it "should submit" do
           expect(company_number_form.submit(valid_params)).to eq(true)
+        end
+
+        context "when the token is less than 8 characters" do
+          before(:each) { valid_params[:company_number] = "946107" }
+
+          it "should increase the length" do
+            company_number_form.submit(valid_params)
+            expect(company_number_form.company_number).to eq("00946107")
+          end
+
+          it "should submit" do
+            expect(company_number_form.submit(valid_params)).to eq(true)
+          end
         end
       end
 
