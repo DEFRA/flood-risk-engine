9db3ad8c539919d6f641f16d9badd50b8d2136ff
Toby Privett <toby@snaplab.co.uk>
2021-09-23 13:59:45 +0100

    Remove deprecated `raise_on_missing_translations`

--- spec/dummy/config/environments/test.rb
+++ spec/dummy/config/environments/test.rb
@@ -36,7 +36,4 @@ Rails.application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
-
-  # Don't raise errors for missing translations
-  config.action_view.raise_on_missing_translations = false
 end
