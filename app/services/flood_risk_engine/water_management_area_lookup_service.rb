409c967e7c7b45a2bfad1c92857e203d8a32d0b6
PaulDoyle-DEFRA <97455399+PaulDoyle-DEFRA@users.noreply.github.com>
2024-04-11 19:27:10 +0100

    Add WaterManagementAreaLookupService

---
+++ app/services/flood_risk_engine/water_management_area_lookup_service.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module FloodRiskEngine
+  class WaterManagementAreaLookupService < BaseService
+    def run(easting:, northing:)
+      unless easting&.to_i&.positive? && northing&.to_i&.positive?
+        raise ArgumentError, "Invalid easting/northing values, integers required"
+      end
+
+      administrative_areas = FloodRiskEngine::WaterManagementArea.arel_table
+      point = RGeo::Cartesian.preferred_factory.point(easting, northing)
+
+      FloodRiskEngine::WaterManagementArea.where(administrative_areas[:area].st_contains(point)).first
+    end
+  end
+end
