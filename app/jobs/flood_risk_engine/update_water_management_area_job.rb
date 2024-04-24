e0ee86ab19bcb8c422651b3af38c59103bc89252
PaulDoyle-DEFRA <97455399+PaulDoyle-DEFRA@users.noreply.github.com>
2024-04-24 17:07:52 +0100

    Fix/ruby 3027 area lookup (#772)
    https://eaflood.atlassian.net/browse/RUBY-3027

--- app/jobs/flood_risk_engine/update_water_management_area_job.rb
+++ app/jobs/flood_risk_engine/update_water_management_area_job.rb
@@ -16,32 +16,12 @@ module FloodRiskEngine
     private
 
     def identify_area
-      response = DefraRuby::Area::WaterManagementAreaService.run(@location.easting, @location.northing)
+      area = FloodRiskEngine::WaterManagementAreaLookupService.run(
+        easting: @location.easting,
+        northing: @location.northing
+      )
 
-      return process_successful_response(response.areas.first) if response.successful?
-
-      process_unsuccessful_response(response)
-    end
-
-    def process_successful_response(area)
-      WaterManagementArea.find_or_create_by(code: area.code) do |water_management_area|
-        water_management_area.update(
-          area_id: area.area_id,
-          area_name: area.area_name,
-          short_name: area.short_name,
-          long_name: area.long_name
-        )
-      end
-    end
-
-    def process_unsuccessful_response(response)
-      return WaterManagementArea.outside_england_area if response.error.instance_of?(DefraRuby::Area::NoMatchError)
-
-      # Any other error we log it and just return nil
-      Airbrake.notify(response.error, easting: @location.easting, northing: @location.northing) if defined? Airbrake
-      Rails.logger.error "Update water management area job failed: #{response.error}"
-
-      nil
+      area || WaterManagementArea.outside_england_area
     end
   end
 end
