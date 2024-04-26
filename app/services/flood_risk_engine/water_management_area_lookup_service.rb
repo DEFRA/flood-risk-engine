4f3d7d287a8b25a13f40188ebc29c980472b8e65
PaulDoyle-DEFRA <97455399+PaulDoyle-DEFRA@users.noreply.github.com>
2024-04-26 12:07:00 +0100

    Fix/easting northing validation (#779)
    https://eaflood.atlassian.net/browse/RUBY-3027

--- app/services/flood_risk_engine/water_management_area_lookup_service.rb
+++ app/services/flood_risk_engine/water_management_area_lookup_service.rb
@@ -3,14 +3,15 @@
 module FloodRiskEngine
   class WaterManagementAreaLookupService < BaseService
     def run(easting:, northing:)
-      unless easting&.to_i&.positive? && northing&.to_i&.positive?
-        raise ArgumentError, "Invalid easting/northing values, integers required"
-      end
+      # Validate the input values
+      Integer(easting) && Integer(northing)
 
       administrative_areas = FloodRiskEngine::WaterManagementArea.arel_table
       point = RGeo::Cartesian.preferred_factory.point(easting, northing)
 
       FloodRiskEngine::WaterManagementArea.where(administrative_areas[:area].st_contains(point)).first
+    rescue ArgumentError, TypeError
+      raise ArgumentError, "Invalid easting/northing values (#{easting}/#{northing}), integers required"
     end
   end
 end
