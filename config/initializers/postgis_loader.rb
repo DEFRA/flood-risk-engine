6b9dea59a9f9fb29ffa9cffda2906dcc97072802
PaulDoyle-DEFRA <97455399+PaulDoyle-DEFRA@users.noreply.github.com>
2025-05-13 17:59:18 +0100

    require postgis/rgeo needed after major version upgrade

---
+++ config/initializers/postgis_loader.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+# These don't autoload as of rgeo-activerecord v8 / activerecord-postgis-adapter v10
+require "rgeo/active_record"
+require "activerecord-postgis-adapter"
